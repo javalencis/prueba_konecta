44c7101a93f591817fb654869e2da9066070d6d6
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2024-10-19 17:32:44 -0500

    Cors agregadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import productsRoute from "./routes/products.routes.js";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/products", productsRoute);
 
