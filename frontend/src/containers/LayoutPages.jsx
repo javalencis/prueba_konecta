ea1aaee9abb4283e6cc72d514c6fac55725acba6
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2024-10-19 17:30:15 -0500

    Agregando lista de productos

--- frontend/src/containers/LayoutPages.jsx
+++ frontend/src/containers/LayoutPages.jsx
@@ -1,12 +1,12 @@
 
 import { Outlet } from "react-router-dom"
 import { LateralMenu } from "../components/LateralMenu"
-
+import '../styles/LayoutPages.scss'
 export const LayoutPages = () => {
   return (
     <section>
         <LateralMenu/>
-        <section>
+        <section className="ContentPage">
             <Outlet/>
         </section>
     </section>
--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,6 +1,13 @@
+import { ProductList } from "../components/ProductList"
 
 export const Products = () => {
   return (
-    <div>Products</div>
+    <>
+      <h2>Productos</h2>
+      <div>
+        <button>Agregar Producto</button>
+      </div>
+      <ProductList/>
+    </>
   )
 }
