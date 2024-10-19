a305e0d32190388d7a87cb8e0699374e30f10b5e
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2024-10-19 14:50:41 -0500

    Conexión base de datos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,20 @@
 import app from "./app.js";
+import { sequelize } from "./database/db.js";
 import "dotenv/config";
 
 const PORT = process.env.PORT ?? 3000;
-app.listen(PORT, () => {
-  console.log("Server on port:", PORT);
-});
+
+async function main() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established succesfully");
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(PORT, () => {
+    console.log("Server on port:", PORT);
+  });
+}
+
+main();
